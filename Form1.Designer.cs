1b1cadea2dcf1130822fa8ee2f9f334ad78fc434
Nahuel <BKP@DESKTOP-OAA37L8>
2024-05-13 20:33:10 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace pryPachecoEtapa3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVehiculo = new System.Windows.Forms.Label();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnBarco = new System.Windows.Forms.Button();
+            this.btnAvion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblVehiculo
+            // 
+            this.lblVehiculo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblVehiculo.Location = new System.Drawing.Point(256, 291);
+            this.lblVehiculo.Name = "lblVehiculo";
+            this.lblVehiculo.Size = new System.Drawing.Size(179, 73);
+            this.lblVehiculo.TabIndex = 4;
+            this.lblVehiculo.Text = "Nombre del vehiculo";
+            this.lblVehiculo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(12, 25);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(120, 53);
+            this.btnAuto.TabIndex = 5;
+            this.btnAuto.Text = "AUTO";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // btnBarco
+            // 
+            this.btnBarco.Location = new System.Drawing.Point(12, 166);
+            this.btnBarco.Name = "btnBarco";
+            this.btnBarco.Size = new System.Drawing.Size(120, 53);
+            this.btnBarco.TabIndex = 6;
+            this.btnBarco.Text = "BARCO";
+            this.btnBarco.UseVisualStyleBackColor = true;
+            this.btnBarco.Click += new System.EventHandler(this.btnBarco_Click);
+            // 
+            // btnAvion
+            // 
+            this.btnAvion.Location = new System.Drawing.Point(12, 311);
+            this.btnAvion.Name = "btnAvion";
+            this.btnAvion.Size = new System.Drawing.Size(120, 53);
+            this.btnAvion.TabIndex = 7;
+            this.btnAvion.Text = "AVION";
+            this.btnAvion.UseVisualStyleBackColor = true;
+            this.btnAvion.Click += new System.EventHandler(this.btnAvion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(668, 450);
+            this.Controls.Add(this.btnAvion);
+            this.Controls.Add(this.btnBarco);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.lblVehiculo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "AUTO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVehiculo;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Button btnBarco;
+        private System.Windows.Forms.Button btnAvion;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryPachecoEtapa3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        clsVehiculo objAuto;
+        clsVehiculo objAvion; //Creamos objeto
+        clsVehiculo objBarco;
+
+        bool Auto = false;
+        bool Avion = false; //Para saber el estado de los vehiculos
+        bool Barco = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objAuto = new clsVehiculo();
+            objAvion = new clsVehiculo(); //Instanciamos objeto
+            objBarco = new clsVehiculo();
+
+            objAuto.CrearAuto();
+            objAvion.CrearAvion(); //Creamos vehiculos
+            objBarco.CrearBarco();
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            objAuto.CrearAuto(); //Creamos de nuevo en caso de que se haya borrado con los otros botones
+            objAuto.pctAuto.Location = new Point(250, 50); //Lugar donde aparece
+            Controls.Add(objAuto.pctAuto); //añadimos imagen
+            lblVehiculo.Text = objAuto.tipoVehiculo;//dictamos que vehiculo es en el label
+            Auto = true;//decimos que ahora existe
+            if (Avion == true)//Borramos los otros vehiculos y los ponemos en falso
+            {
+                objAvion.pctAvion.Dispose();
+                Avion = false;
+            }
+            if (Barco == true)
+            {
+                objBarco.pctBarco.Dispose();
+                Barco = false;
+            }
+        }
+
+        private void btnBarco_Click(object sender, EventArgs e)
+        {
+            objAvion.CrearBarco();
+            objBarco.pctBarco.Location = new Point(250, 50);
+            Controls.Add(objBarco.pctBarco);
+            lblVehiculo.Text = objBarco.tipoVehiculo;
+            Barco = true;
+            if (Avion == true)
+            {
+                objAvion.pctAvion.Dispose();
+                Avion = false;
+            }
+            if (Auto == true)
+            {
+                objAuto.pctAuto.Dispose();
+                Auto = false;
+            }
+        }
+
+        private void btnAvion_Click(object sender, EventArgs e)
+        {
+            objAvion.CrearAvion();
+            objAvion.pctAvion.Location = new Point(250, 50);
+            Controls.Add(objAvion.pctAvion);
+            lblVehiculo.Text = objAvion.tipoVehiculo;
+            Avion = true;
+            if (Auto == true)
+            {
+                objAuto.pctAuto.Dispose();
+                Auto = false;
+            }
+            if (Barco == true)
+            {
+                objBarco.pctBarco.Dispose();
+                Barco = true;
+            }
+        }
+    }
+}
